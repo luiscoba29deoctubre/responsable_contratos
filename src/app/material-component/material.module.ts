bf159fc979315aa22f351ed4f9ebbaa4c186596b
Luis <luiscoba88@yahoo.com>
2020-10-05 11:55:01 -0500

    se inicia la subida del archivo

--- src/app/material-component/material.module.ts
+++ src/app/material-component/material.module.ts
@@ -16,7 +16,9 @@ import { ParametricaComponent } from "./parametrica/parametrica.component";
 import { DialogBoxComponent } from "./dialog-box/dialog-box.component";
 import { DialogExitComponent } from "./dialog-exit/dialog-exit.component";
 import { DialogAcceptComponent } from "./dialog-accept/dialog-accept.component";
-import { ReporteComponent } from './reporte/reporte.component';
+import { ReporteComponent } from "./reporte/reporte.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { AngularFileUploaderModule } from './reporte/angular-file-uploader/src/lib/angular-file-uploader.module';
 
 @NgModule({
   imports: [
@@ -28,6 +30,8 @@ import { ReporteComponent } from './reporte/reporte.component';
     ReactiveFormsModule,
     FlexLayoutModule,
     CdkTableModule,
+
+    AngularFileUploaderModule,
   ],
   providers: [],
   entryComponents: [],
@@ -38,5 +42,6 @@ import { ReporteComponent } from './reporte/reporte.component';
     DialogAcceptComponent,
     ReporteComponent,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MaterialComponentsModule {}
